9d44ce827ea0af5e0f1b01da1724d3754f482afa
Michael Rogenmoser <michael@rogenmoser.us>
2022-01-18 09:08:43 +0100

    Fix VSIM compatibility for different versions

--- hardware/vsim/start_sim.sh
+++ hardware/vsim/start_sim.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 set -e
+if [ -z "$VSIM" ]; then
+    VSIM="vsim-10.7b"
+fi
+readonly VSIM
 
 if [ -n "$CI" -o -z "$DISPLAY" ]; then
     # Run in console-only mode.
-    vsim-10.7b -c -do 'source run.tcl; quit -code $quitCode'
+    ${VSIM} -c -do 'source run.tcl; quit -code $quitCode'
 else
     # Run in GUI mode and silence console output.
-    vsim-10.7b -do 'source run.tcl' &>/dev/null
+    ${VSIM} -do 'source run.tcl' &>/dev/null
 fi
